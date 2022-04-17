1832be32704cdc362860cf89d99978d352dfddfe
Anatoliy Maximichev <84544880+MetallistOld@users.noreply.github.com>
2022-04-17 16:39:40 +0300

    Lesson 1 (#1)

---
+++ todo/users/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from users.models import User
+from users.serializers import UserModelSerializer
+
+
+class UserModelViewSet(ModelViewSet):
+    """
+    Provides actions to list, retrieve, create, update or destroy User model/-s
+    """
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
